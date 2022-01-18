49bd4d02d91a6f6ee630111ad6e163888521cd26
SasivimolB <pangsasi.mol@gmail.com>
2022-01-18 15:48:30 +0700

    file upload

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -7,17 +7,75 @@ export default function Register() {
     const [password, setPassword] = useState();
     const [firstname, setFirstname] = useState();
     const [lastname, setLastname] = useState();
-    const [file, setFile] = useState();
+    // const [file, setFile] = useState();
+    // const [filename, setFilename] = useState();
+    var checkUser = true;
 
-    const addUser = () => {
-        console.log(username);
-        Axios.post("http://localhost:3001/regis", {
-            username: username, 
-            password: password
-        }).then((response) => {
-            console.log('success');
-        });
+    function checkUsername() {
+        var re = /^\w+$/;
+        if(!(username.match(re)))
+        {
+            alert('Please use only alphabet characters(A-Z, a-z), numbers(0-9), and underscore(_) for username');
+            checkUser = false;
+        }
+        if(username.length>12) {
+            alert('Username must be shorter than 12 characters');
+            checkUser = false;
+        }
     }
+
+    function checkPassword() { 
+        var re = /[a-zA-Z]{2,}|\d{2,}/g;
+        if(password.length<6) {
+            alert('Password must be longer than 6 characters');
+            checkUser = false;
+        }
+        if(password.match(re)) {
+            alert("There shouldn't be any consecutive alphabets or numbers in the password.");
+            checkUser = false;
+        }
+    }
+
+    const addUser = async () => {
+        checkPassword();
+        checkUsername();
+        if(checkUser === true)
+        {
+            console.log(username);
+            const formdata = new FormData(); 
+            formdata.append('username', username);
+            formdata.append('password', password);
+            formdata.append('firstname', firstname);
+            formdata.append('lastname', lastname);
+            formdata.append('profilepic', userInfo.file);
+            Axios.post("http://localhost:3001/regis", formdata, {
+                headers: { "Content-Type": "multipart/form-data, boundary=${form._boundary}" }
+            }).then((response) => {
+                console.log('success');
+            });
+        }
+        
+    }
+
+    // const submit = async () =>{
+    //     const formdata = new FormData(); 
+    //     formdata.append('profilepic', userInfo.file);
+    
+    //     Axios.post("http://localhost:3001/regis", formdata,{   
+    //       headers: { "Content-Type": "multipart/form-data" } 
+    //     })
+    //     .then(res => { // then print response status
+    //       console.warn(res);
+    //       if(res.data.success === 1){
+    //         console.log("Image upload successfully");
+    //       }
+    //     })
+    // }
+
+    const [userInfo, setuserInfo] = useState({
+        file:[],
+        filepreview:null,
+    });
 
     return (
         <div>
@@ -38,11 +96,17 @@ export default function Register() {
                 setLastname(event.target.value);
             }}/><br/>
             <input type="file" onChange={(event => {
-                setFile(event.target.value);
+                // setFilename(event.target.files[0].name)
+                // setFile(event.target.files[0])
+                setuserInfo({
+                    ...userInfo,
+                    file:event.target.files[0],
+                    filepreview:URL.createObjectURL(event.target.files[0]),
+                });
             })}/><br/>
             <br/>
             <button onClick={addUser}>Register</button><br/>
-            Already have an account? <Link to="/">Log in</Link>
+            Already have an account? <Link to="/login">Log in</Link>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express()
 const mysql = require('mysql')
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const multer = require('multer')
+const path = require('path');
 
 app.use(cors());
 app.use(express.json())
@@ -11,28 +15,109 @@ const db = mysql.createConnection({
     user:'root',
     host: 'localhost',
     password: 'root',
-    database: 'try1'
+    database: 'webappdb'
 })
 
-app.post('/regis', (req, res) => {
-    console.log(req.body.username);
-    const username = req.body.username;
-    const password = req.body.password;
+db.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+})
 
-    bcrypt.hash(password, 10, function(err, hash) {
-        db.query(
-            'INSERT INTO users(username, password) VALUES (?, ?)',
-            [username, hash], 
-            (err, result) => {
-                if(err) {
-                    console.log(err);
-                }
-                else {
-                    res.send("Values Inserted");
-                }
-            }
-        );
-    });    
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, './public_html/', 'uploads'),
+    filename: function (req, file, cb) {   
+        cb(null, Date.now() + '-' + file.originalname )  
+    }
+})
+
+const upload = multer({ storage: storage}).single('profilepic');
+
+app.post('/regis', upload, (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+    console.log(req.file.filename);
+
+    // try {
+        
+    //     // db.query(
+    //     //     'INSERT INTO userinfo(username, firstname, lastname, profilepic) VALUES (?, ?, ?, ?)',
+    //     //     [username, firstname, lastname, profilepic], 
+    //     //     (err, result) => {
+    //     //         if(err) {
+    //     //             console.log(err);
+    //     //         }
+    //     //         else {
+    //     //             res.send("Values Inserted");
+    //     //         }
+    //     //     }
+    //     // );
+    //     // upload(req, res, function(err) {
+    //     //     // req.file contains information of uploaded file
+    //     //     // req.body contains information of text fields
+    //     //     const imageName = { image: req.file.filename };
+    //     //     // var sql ='INSERT INTO users SET ?'
+    //     //     var userid = 0;
+    //     //     db.query(
+    //     //         'SELECT * FROM userinfo WHERE username=userinfo.username',
+    //     //         (err, result) => {
+    //     //             console.log(result[0]);
+    //     //             if(result[0].length) {
+    //     //                 console.log("The username is already exists.");
+    //     //             }
+    //     //             else {
+    //     //                 console.log("Proceed to info. insertion...")
+    //     //                 bcrypt.hash(password, 10, function(err, hash) {
+    //     //                     db.query(
+    //     //                         'INSERT INTO userinfo(username, firstname, lastname, profilepic) VALUES (?, ?, ?, ?)',
+    //     //                         [username, firstname, lastname, imageName], 
+    //     //                         (err, result) => {
+    //     //                             if(err) {
+    //     //                                 console.log(err);
+    //     //                             }
+    //     //                             else {
+    //     //                                 res.send("Values Inserted");
+    //     //                             }
+    //     //                         }
+    //     //                     );
+    //     //                     db.query(
+    //     //                         'SELECT id FROM userinfo WHERE username = username',
+    //     //                         (err, res) => {
+    //     //                             if(err) {
+    //     //                                 console.log(err);
+    //     //                             }
+    //     //                             else {
+    //     //                                 userid = res[0];
+    //     //                                 console.log(userid);
+    //     //                             }
+    //     //                         }
+    //     //                     )
+    //     //                     db.query(
+    //     //                         'INSERT INTO userpw(id, password) VALUES(?, ?)',
+    //     //                         [userid, hash],
+    //     //                         (err, res) => {
+    //     //                             if(err) {
+    //     //                                 console.log(err);
+    //     //                             }
+    //     //                             else {
+    //     //                                 res.send("Password Inserted");
+    //     //                             }
+    //     //                         }
+    //     //                     );
+    //     //                 });
+    //     //             }
+    //     //         }
+    //     //     )
+    //     //     // db.query("INSERT INTO users SET ?", imageName, 
+    //     //     //     (err, results) => {  
+    //     //     //         if (err) throw err;
+	// 	// 	// 	    res.json({ success: 1 })
+	// 	// 	// });  
+    //     // }); 
+    // }catch (err) {console.log(err)}
 })
 
 app.post('/login', (req, res)=> {
@@ -42,16 +127,28 @@ app.post('/login', (req, res)=> {
     if (username && password) {
         console.log(username);
         db.query(
-            'SELECT password FROM users WHERE username = ? ORDER BY id DESC LIMIT 1',
+            'SELECT id, password FROM users WHERE username = ? ORDER BY id DESC LIMIT 1',
             [username],
             (err, res) => {
                 if(err) {
                     console.log(err);
                 }
                 else {
-                    //console.log(res[0].password)
-                    if(bcrypt.compareSync(password, res[0].password)) {
+                    //console.log(res[1].password)
+                    if(bcrypt.compareSync(password, res[1].password)) {
                         console.log("matching")
+                        const token = jwt.sign({
+                            userId: result[0].id
+                            },
+                            'SECRETKEY', {
+                            expiresIn: '180000'
+                            }
+                        );
+                        return res.status(200).send({
+                            msg: 'Logged in!',
+                            token,
+                            user: result[0]
+                        });
                     }
                     else {
                         console.log("not match")
@@ -61,6 +158,15 @@ app.post('/login', (req, res)=> {
         )
     }
 });
+
+app.post('/edit-profile', (req, res) => {
+    // const username = req.body.username;
+    // const password = req.body.password;
+    // const firstname = req.body.firstname;
+    // const lastname = req.body.lastname;
+    //const profilepic = req.body.profilepic;
+
+})
 
 app.listen(3001, () => {
     console.log("Server is running...");
